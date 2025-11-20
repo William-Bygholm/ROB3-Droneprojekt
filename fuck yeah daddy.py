a94ba86cfd8bf0b6218800cb548ae1925c3e2687
Elias101002 <ehagedornhansen@gmail.com>
2025-11-20 15:28:29 +0100

    Update fuck yeah daddy.py

--- fuck yeah daddy.py
+++ fuck yeah daddy.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.resize(cv2.imread('Billeder_af_Olaf_tismand _+_torch/IMG_20251104_094347.jpg'), (0,0), fx=0.1, fy=0.1)
+img = cv2.resize(cv2.imread('Billeder_af_Olaf_tismand _+_torch/IMG_20251104_094347.jpg'), (0,0), fx=0.3, fy=0.3)
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
