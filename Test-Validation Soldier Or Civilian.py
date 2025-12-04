dc35b07cd5173756cd3b98a1c8612c66d9db6bb5
William-Bygholm <wholm.skole@gmail.com>
2025-12-04 17:18:23 +0100

    Ye

--- Test-Validation Soldier Or Civilian.py
+++ Test-Validation Soldier Or Civilian.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 VIDEO_PATH = "ProjektVideoer/2 militær med blå bånd .MP4"
 COCO_JSON = "Validation/2 mili med blå bond.json"
-THRESHOLD_SCORE = 0.5
+THRESHOLD_SCORE = 0.8
 
 def compute_histogram(img, center_y_ratio=0.35, center_x_ratio=0.5, height_ratio=0.2, width_ratio=0.3):
     """
