2d9e198d3d00217c6d42f9275674a89f2a40c20f
Olaf A' Grønastød <olafgronastod1@gmail.com>
2025-12-04 10:18:21 +0100

    j

--- Test find_armbond.py
+++ Test find_armbond.py
@@ -190,8 +190,8 @@ def crop_top_of_roi(roi):
 def edge_detection(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    sobelx = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=5)
-    sobely = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=5)
+    sobelx = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
 
     gradient_magnitude = cv2.magnitude(sobelx, sobely)
 
