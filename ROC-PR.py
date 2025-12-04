fa368a193da2d0628d649223bd8d54ceff256772
AlexHolt123 <alexander.p.holt@gmail.com>
2025-12-04 13:09:05 +0100

    fafa

--- ROC-PR.py
+++ ROC-PR.py
@@ -11,12 +11,12 @@ VIDEO_IN = r"C:\Users\alexa\Documents\GitHub\ROB3-Droneprojekt\ProjektVideoer\2 
 JSON_COCO = r"C:\Users\alexa\Documents\GitHub\ROB3-Droneprojekt\Validation\2 mili med blå bond.json"
 MODEL_FILE = "Person_Detector_Json.pkl"
 
-SCALES = [1.0, 0.8, 0.64, 0.5]
-STEP_SIZES = {1.0: 48, 0.8: 36, 0.64: 24, 0.5: 16}
+SCALES = [1.0, 0.8, 0.64]
+STEP_SIZES = {1.0: 48, 0.8: 36, 0.64: 24}
 NMS_THRESHOLD = 0.05
 FRAME_SKIP = 2
 WINDOW_SIZE = (128, 256)
-IOU_POSITIVE = 0.5
+IOU_POSITIVE = 0.3
 
 # ---------------- LOAD MODEL ----------------
 print("Loading model...")
