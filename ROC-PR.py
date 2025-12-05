9b701c092f5648fb000580550b441ecba70ac79a
AlexHolt123 <alexander.p.holt@gmail.com>
2025-12-05 14:32:30 +0100

    ja12

--- ROC-PR.py
+++ ROC-PR.py
@@ -9,7 +9,7 @@ from sklearn.metrics import roc_curve, precision_recall_curve, auc, f1_score
 # ---------------- USER CONFIG ----------------
 VIDEO_IN = r"C:\Users\alexa\Documents\GitHub\ROB3-Droneprojekt\ProjektVideoer\2 militær med blå bånd .MP4"
 JSON_COCO = r"C:\Users\alexa\Documents\GitHub\ROB3-Droneprojekt\Validation\2 mili med blå bond.json"
-MODEL_FILE = "Person_Detector_Json+YOLO.pkl"
+MODEL_FILE = "Person_Detector_Json.pkl"
 
 SCALES = [1.2, 1, 0.8, 0.64]
 STEP_SIZES = {1.2: 36, 1: 34, 0.8: 28, 0.64: 24}
